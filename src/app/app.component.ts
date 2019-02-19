9dd8bee72ca842809afa66603431ca6d55180852
John Mäki <john.maki@hotmail.com>
2019-02-19 21:36:52 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // tslint:disable-next-line:component-selector
+  selector: 'app',
+  templateUrl: 'app.component.html'
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 }
